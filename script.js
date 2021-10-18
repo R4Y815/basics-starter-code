10b6fe694e011004c6481a693032334342d4e8a9
R4Y815 <Akcent360@gmail.com>
2021-10-18 14:35:30 +0800

    MoreComfortable Exercise 2Dice with 4D- completed

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
+//4D string generator function. Placed globally so 4D number stays the same until webpage is reloaded, so players can keep trying to Submit
+
+var random4Dstring = function () {
+  var randomDecimal01 = Math.random() * 10;
+  var randomDigit01 = Math.floor(randomDecimal01);
+  var randomDecimal02 = Math.random() * 10;
+  var randomDigit02 = Math.floor(randomDecimal02);
+  var randomDecimal03 = Math.random() * 10;
+  var randomDigit03 = Math.floor(randomDecimal03);
+  var randomDecimal04 = Math.random() * 10;
+  var randomDigit04 = Math.floor(randomDecimal04);
+  var string4Digit =
+    randomDigit01.toString() +
+    randomDigit02.toString() +
+    randomDigit03.toString() +
+    randomDigit04.toString();
+  return string4Digit; // ----- INPUT CONTROL POINT 1 OF 4
+};
+
 var random1to3 = function () {
   var randomDecimal = Math.random() * 3;
   var randomInteger = Math.floor(randomDecimal); //
-  return randomInteger + 1;
+  return randomInteger + 1; // ----- INPUT CONTROL POINT 2 OF 4
 };
 
 var diceRoll = function () {
@@ -15,32 +34,110 @@ var diceRoll = function () {
 
 var withInNmber = random1to3();
 var gameNumber = 0;
+var diceTotalScore = 0;
+var fourDstring = random4Dstring();
+var fourDnumber = Number(fourDstring);
+var gameMode = 1; // gameMode = 1 -DiceRoll gameMode= 2 : 4D
+
 var main = function (input) {
   gameNumber = gameNumber + 1;
-  console.log("##### GAME NUMBER #####");
+  console.log("# GAME NUMBER #####");
   console.log(gameNumber);
+  console.log("** GAME MODE NOW **");
+  console.log(gameMode);
   console.log(">>> withInNmber in Current Round = ");
   console.log(withInNmber);
-  var randomDiceRoll1 = diceRoll();
-  var randomDiceRoll2 = diceRoll();
-  console.log(">>>>>  Dice Rolled");
+  var randomDiceRoll1 = 4; // ----- INPUT CONTROL POINT 3 OF 4
+  var randomDiceRoll2 = 6; // ----- INPUT CONTROL POINT 4 OF 4
+  console.log(">>>>>  Dices Rolled");
   console.log(randomDiceRoll1);
-  var myOutputValue = `For this round, you win if your guesss is within ${withInNmber} of either one of the dice rolls. <br><br> You guessed ${input}.<br><br>.You rolled a ${randomDiceRoll1} and a ${randomDiceRoll2}. <br><br> >>>>> You lose, please try again. `;
+  console.log(randomDiceRoll2);
 
-  var winMsg = `For this round, you win if your guesss is within ${withInNmber} of either one of the dice rolls. <br><br> You guessed ${input}.<br><br>.You rolled a ${randomDiceRoll1} and a ${randomDiceRoll2}. <br><br> >>>>> YOU WIN! <br><br>The difficulty will now be changed. `;
+  var myOutputValue = `Wrong Input. Please try again`;
 
+  var loseMsg = `GAME: Dice Within Range, 2 correct guesses in a roll to win.<br><br>YOU LOSE<----<br><br> To win, your guess must be within ${withInNmber} for either one of the 2 dice rolls twice in a roll. <br><br> Your guess: ${input}.<br>Your 2 dice rolls: ${randomDiceRoll1} & ${randomDiceRoll2}.<br><br>Please try again.`;
+
+  var winMsg = `GAME: Dice Within Range, 2 correct guesses in a roll to win.<br><br> Correct Guess! You need 1 more correct guess to win this dice game. <br><br> Your guess: ${input} <br>is within ${withInNmber} of  your two dice rolls:<br> ${randomDiceRoll1} & ${randomDiceRoll2}. `;
+
+  var bonusRoundInvite = `GAME: Dice Within Range, 2 correct guesses in a roll to win.<br><br>.You have won the dice game twice in a row.<br><br> >>>>>>>>> BONUS ROUND: Please guess a 4 digit number:`;
+
+  var bonusRoundLose = `GAME: Guess 4D number <br><br>YOU LOSE !! <br><br> ${input} is not the 4D number <br> Try again.`;
+
+  var bonusRoundWin = `GAME: Guess 4D number <br><br> YOU WIN!! <br><br>You guessed: ${input} <br> Actual 4D number:${fourDstring}<br><br> Next Game: <br>Dice Within Range, 2 correct guesses in a roll to win. `;
+
+  var differenceRoll01 = Math.abs(input - randomDiceRoll1);
+  var differenceRoll02 = Math.abs(input - randomDiceRoll2);
+
+  // Conditions for winning the dice game. note that we use <= to also include the limits.
   if (
-    input == randomDiceRoll1 ||
-    (input > randomDiceRoll1 && input - randomDiceRoll1 <= withInNmber) ||
-    (randomDiceRoll1 > input && randomDiceRoll1 - input <= withInNmber) ||
-    input == randomDiceRoll2 ||
-    (input > randomDiceRoll2 && input - randomDiceRoll2 <= withInNmber) ||
-    (randomDiceRoll2 > input && randomDiceRoll2 - input <= withInNmber)
+    (gameMode == 1 && input == randomDiceRoll1) ||
+    (gameMode == 1 && input == randomDiceRoll2) ||
+    (gameMode == 1 &&
+      (differenceRoll01 <= withInNmber || differenceRoll02 <= withInNmber))
   ) {
+    diceTotalScore = diceTotalScore + 1;
     myOutputValue = winMsg;
-    withInNmber = random1to3();
   }
+
+  //When playing Dice Game , reset Dice Score Counter when players fail on second roll. Note that we will need to reset the total dice score to zero for eaach time the dice guess is wrong because we need 2 ceonsecutive correct guesses to win
+  if (
+    gameMode == 1 &&
+    differenceRoll01 > withInNmber &&
+    differenceRoll02 > withInNmber
+  ) {
+    diceTotalScore = 0;
+    myOutputValue = loseMsg;
+  }
+
+  if (gameMode == 2 && input > fourDnumber && diceTotalScore >= 2) {
+    myOutputValue = bonusRoundLose;
+  }
+
+  if (gameMode == 2 && input < fourDnumber && diceTotalScore >= 2) {
+    myOutputValue = bonusRoundLose;
+    differenceRoll01 = "not used";
+    differenceRoll02 = "not used";
+  }
+
+  if (gameMode == 1 && diceTotalScore == 2) {
+    myOutputValue = bonusRoundInvite;
+    gameMode = 2;
+  }
+
+  var randomiseWithInNmber = random1to3();
+  var nextFourDstring = random4Dstring();
+  var nextFourDnumber = Number(nextFourDstring);
+
+  if (gameMode == 2 && input == fourDnumber) {
+    myOutputValue = bonusRoundWin;
+    withInNmber = randomiseWithInNmber;
+    diceTotalScore = 0;
+    gameNumber = 0;
+    gameMode = 1;
+    fourDstring = nextFourDstring;
+    fourDnumber = nextFourDnumber;
+    differenceRoll01 = "not used";
+    differenceRoll02 = "not used";
+  }
+  console.log("// Difference Dice Roll 1 and input");
+  console.log(differenceRoll01);
+
+  console.log("// Difference Dice Roll 2 and input");
+  console.log(differenceRoll02);
+
+  console.log(" ** DICE GAME TOTAL SCORE **");
+  console.log(diceTotalScore);
+
   console.log(">>> withInNmber FOR NEXT Round = ");
   console.log(withInNmber);
+
+  console.log(">>>> 4D String = ");
+  console.log(fourDstring);
+
+  console.log("** NEXT GAME MODE **");
+  console.log(gameMode);
+
+  console.log("______________________");
+  console.log("______________________");
   return myOutputValue;
 };
